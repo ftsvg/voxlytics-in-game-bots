4f737f1744f0dce4666ffdc9c76d02490b629525
Ventros <265562747+ftsvg@users.noreply.github.com>
2026-06-23 18:38:35 +0200

    test: delays

--- denicker/constants.js
+++ denicker/constants.js
@@ -1,9 +1,9 @@
-export const EVENT_HISTORY_TIME = 50
+export const EVENT_HISTORY_TIME = 10000
 export const MIN_NICK_DELAY = 0
-export const MAX_NICK_DELAY = 5
+export const MAX_NICK_DELAY = 3000
 
-export const PING_CHECK_DELAY = 5
-export const PING_TOLERANCE = 10
+export const PING_CHECK_DELAY = 3000
+export const PING_TOLERANCE = 30
 
 export const COLORS = {
     UNNICK: 0xFF5555,
